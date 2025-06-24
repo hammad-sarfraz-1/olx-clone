14f324cc361a5068851bf288a8636d41d8d3bce4
hammad-sarfraz-1 <i211748@nu.edu.pk>
2025-06-24 18:57:35 +0500

    Added tests

--- olx/users/tests.py
+++ olx/users/tests.py
@@ -1,3 +1,18 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 # Create your tests here.
+User = get_user_model()
+
+
+class UserModelTests(TestCase):
+    def test_user_creation(self):
+        user = User.objects.create_user(
+            username="hammad",
+            password="securepass",
+            email="hammad@example.com",
+            phone_number="1234567890",
+        )
+        self.assertEqual(user.username, "hammad")
+        self.assertTrue(user.check_password("securepass"))
+        self.assertEqual(str(user), "hammad")
